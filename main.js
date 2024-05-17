a505d26c5427955021819d1aa7e7c65f0fed3a0a
Shashwat Suman <shashwatsumanat29@gmail.com>
2024-05-17 20:09:35 +0530

    updated js for functionality

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+//api key from newsAPI
 const apiKey = "a7fab18e28b04043bcfd32a1403ca202";
 
 const blogContainer = document.getElementById("blog-container");
 
+const searchField = document.getElementById("search-input");
+
+const searchButton = document.getElementById("search-btn");
+
+// fetching news articles through api key
 async function fetchRandomNews() {
   try {
     const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=${apiKey}`;
@@ -15,6 +21,36 @@ async function fetchRandomNews() {
   }
 }
 
+// when user clicks anything on search box then it will show articles related to that.
+searchButton.addEventListener("click",  async ()=>{
+    const query = searchField.value.trim()
+    if(query !== ""){
+        try{
+            const articles = await fetchNewsQuery(query)
+            displayBlogs(articles)
+        }catch(error){
+            console.log('Error fetching news by query', error)
+        }
+    }
+
+})
+
+ async function fetchNewsQuery(query){
+    try {
+        const apiUrl = `https://newsapi.org/v2/everything?q=${query}&pageSize=10&apikey=${apiKey}`;
+        const response = await fetch(apiUrl);
+    
+        const data = await response.json();
+        return data.articles;
+      } catch (error) {
+        console.error("Error fetching random news", error);
+        return [];
+      }
+}
+
+
+// creating html cards using js
+ 
 function displayBlogs(articles) {
   blogContainer.innerHTML = "";
   articles.forEach((articles) => {
@@ -33,22 +69,26 @@ function displayBlogs(articles) {
     title.textContent = updatedTitle;
 
     const description = document.createElement("p");
-    const updatedDes = articles.description 
-      ? (articles.description.length > 120
-          ? articles.description.slice(0, 120) + "..."
-          : articles.description)
+    const updatedDes = articles.description
+      ? articles.description.length > 120
+        ? articles.description.slice(0, 120) + "..."
+        : articles.description
       : "No description available";
     description.textContent = updatedDes;
-    
 
     blogCard.appendChild(img);
-    
     blogCard.appendChild(title);
     blogCard.appendChild(description);
+
+    // when user clicks on any card, then it will open the news article in new window.
+    blogCard.addEventListener("click", ()=>{
+        window.open(articles.url, "_blank");
+    });
     blogContainer.appendChild(blogCard);
   });
 }
 
+// it display some pre loaded news when user visit the website.
 (async () => {
   try {
     const articles = await fetchRandomNews();
